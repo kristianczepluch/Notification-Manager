5ea4a3f02dc34bae7a1c301d62829b20f0de459a
Kristian Czepluch <czepluch.kristian@gmail.com>
2020-03-25 03:25:43 +0100

    Set up the a viewpager 2 as the main interface with three different fragments

--- NotificationManager/app/src/main/java/com/example/notificationmanager/MainActivity.kt
+++ NotificationManager/app/src/main/java/com/example/notificationmanager/MainActivity.kt
@@ -3,16 +3,53 @@ package com.example.notificationmanager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.example.notificationmanager.fragments.ManipulatedNotificationsFragment
+import com.example.notificationmanager.fragments.NotificationOverview
+import com.example.notificationmanager.fragments.NotificationRules
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mainToolbar: Toolbar
+    lateinit var viewPager: ViewPager2
+    lateinit var myTabLayout: TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Setup the toolbar
         mainToolbar = findViewById(R.id.main_toolbar)
         setSupportActionBar(mainToolbar)
+
+        // Setup the viewPager
+        viewPager = findViewById(R.id.main_viewpager)
+        viewPager.adapter = SimpleViewPagerAdapter(supportFragmentManager, lifecycle)
+        myTabLayout = findViewById(R.id.main_tablayout)
+        TabLayoutMediator(myTabLayout, viewPager){ tab, position ->
+            when(position){
+                0 -> tab.text = getString(R.string.tab_name_0)
+                1 -> tab.text = getString(R.string.tab_name_1)
+                2 -> tab.text = getString(R.string.tab_name_2)
+            }
+        }.attach()
+
+    }
+}
+
+class SimpleViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager, lifecycle){
+
+    private val fragement_list = listOf(NotificationOverview(), NotificationRules(), ManipulatedNotificationsFragment())
+
+    override fun getItemCount() = fragement_list.size
+
+    override fun createFragment(position: Int): Fragment {
+        return fragement_list[position]
     }
 }
---
+++ NotificationManager/app/src/main/java/com/example/notificationmanager/fragments/ManipulatedNotificationsFragment.kt
@@ -0,0 +1,20 @@
+package com.example.notificationmanager.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.notificationmanager.R
+
+class ManipulatedNotificationsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_manipulated_notifications, container, false)
+    }
+}
