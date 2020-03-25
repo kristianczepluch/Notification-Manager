3d0d5699eef46159a125dd72938fd6439b500aee
Kristian Czepluch <czepluch.kristian@gmail.com>
2020-03-25 01:57:32 +0100

    get rid off actionbar and add a toolbar instead

--- NotificationManager/app/src/main/java/com/example/notificationmanager/MainActivity.kt
+++ NotificationManager/app/src/main/java/com/example/notificationmanager/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.notificationmanager
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainToolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainToolbar = findViewById(R.id.main_toolbar)
+        setSupportActionBar(mainToolbar)
     }
 }
