5aabf35dca9eb5a04a16354dd49689480c202ca4
Kristian Czepluch <czepluch.kristian@gmail.com>
2020-05-26 18:10:03 +0200

    Clean up: Hardcoded string-values and dead-ressources

--- NotificationManager/app/src/main/java/com/example/notificationmanager/fragments/HeaderRuleWizardFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.example.notificationmanager.fragments
-
-import androidx.fragment.app.Fragment
-import com.example.notificationmanager.R
-
-
-class HeaderRuleWizardFragment : Fragment(R.layout.fragment_header_rule_wizard) {
-
-}
